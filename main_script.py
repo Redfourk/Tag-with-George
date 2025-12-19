a0c521cb4052247be06597b17289a23de14a6c6f
Redfourk <turnej2096@g.comalisd.org>
2025-12-19 01:15:20 +0000

    Add player 2 and clean up

--- main_script.py
+++ main_script.py
@@ -1,44 +1,63 @@
-#import pygame
-#import pynput
-import arcade
-#from pygame import display
-# from pygame.event import Event
-# from pyglet import window
-# from pyglet.input.linux.evdev_constants import KEY_LEFT
-# from pynput import keyboard
-import tkinter as tk
-
-# print(f"Pygame imported successfully, version: {pygame.version.ver}")
-# print("Pynput keyboard module imported successfully")
-
-# Stuff to figure out later...
-
+#===================================================================================
+#
+# Tag with George by Redfourk and CatthonCoder
+# Copyright © 2025 Redfourk
+# This code uses the MIT license, for more information, see https://mit-license.org/
+#
+#===================================================================================
+#
+#
+#       ~Chore list~
+# Todo: Translate audio mechanics:
 # my_music = html.AUDIO(src="https://codehs.com/uploads/8179d3793582c02c44438d0160d6f0a5")
 # my_music.loop = True
 # my_music
 # .play()
+# Todo: Implement player 2
+# Todo: Find updated features added by CatthonCoder
+# Todo: Clean up and format code
+# Todo: Implement bushes
+#
+#
+#
+#       ~~~Code Starts Here~~~
 
+# Import necessary resources
+
+import tkinter as tk
+import threading
+
+
+
+from pyglet import event
+from pyglet.media import player
+
+# Create application window. App Name: "window"
+# Line below MUST be first tkinter related code to execute!
 window = tk.Tk()
-
-player1_x = 350
-player1_y = 200
-
 
 window.title("Tkinter window")
 window.geometry("2000x1200")
 label = tk.Label(window, text="Tag with George v0.1.0")
-#shape_name = tk.Canvas(app_name, width=__, height=__, bg= "lowercase")
+
+# Create app backdrop (technically a rectangle). Shape creation format: shape_name = tk.Canvas(app_name, width=__, height=__, bg= "lowercase")
+
 background = tk.Canvas(window, width=2000, height=1200, bg="green")
-x0 = 1740
-y0 = 590
-x1 = 1760
-y1 = 610
-player1 = tk.Canvas.create_oval(background, x0, y0, x1, y1, outline="red", fill="red", width=15)
+
+# Create arrow-key player, referenced as "player1"
+
+player1_x0 = 1740
+player1_y0 = 590
+player1_x1 = 1760
+player1_y1 = 610
+player1 = tk.Canvas.create_oval(background, player1_x0, player1_y0, player1_x1, player1_y1, outline="red", fill="red", width=15)
+
+# Add movement script with keybinds for player1 (Up Arrow, Down Arrow, Left Arrow, Right Arrow)
+# NOTE: Move left process must refresh screen, otherwise ghosting will occur
 
 STEP = 10
 
-def move_circle(event):
-    """Moves the circle based on the arrow key pressed."""
+def move_player1(event):
     if event.keysym == "Up":
         background.move(player1, 0, -STEP)
     elif event.keysym == "Down":
@@ -49,19 +68,48 @@ def move_circle(event):
     elif event.keysym == "Right":
         background.move(player1, STEP, 0)
 
-window.bind('<Up>', move_circle)
-window.bind('<Down>', move_circle)
-window.bind('<Left>', move_circle)
-window.bind('<Right>', move_circle)
-window.bind('<w>', move_circle)
-window.bind('<a>', move_circle)
-window.bind('<s>', move_circle)
-window.bind('<d>', move_circle)
+window.bind('<Up>', move_player1)
+window.bind('<Down>', move_player1)
+window.bind('<Left>', move_player1)
+window.bind('<Right>', move_player1)
 
-background.focus_set()
+# Create WASD player, referenced as "player2"
+
+player2_x0 = 870
+player2_y0 = 590
+player2_x1 = 890
+player2_y1 = 610
+player2 = tk.Canvas.create_oval(background, player2_x0, player2_y0, player2_x1, player2_y1, outline="white", fill="white", width=15)
+
+def move_player2(event):
+    if event.keysym == "W" or event.keysym == "w":
+        background.move(player2, 0, -STEP)
+    elif event.keysym == "S" or event.keysym == "s":
+        background.move(player2, 0, STEP)
+    elif event.keysym == "A" or event.keysym == "a":
+        background.move(player2, -STEP, 0)
+        background.update_idletasks()
+    elif event.keysym == "D" or event.keysym == "d":
+        background.move(player2, STEP, 0)
+
+window.bind('<W>', move_player2)
+window.bind('<S>', move_player2)
+window.bind('<A>', move_player2)
+window.bind('<D>', move_player2)
+
+window.bind('<w>', move_player2)
+window.bind('<s>', move_player2)
+window.bind('<a>', move_player2)
+window.bind('<d>', move_player2)
+
+
+
+
+window.focus_set()
 label.pack()
 background.pack()
 window.mainloop()
+
 
 # 350, 200 out of 400x400
 # 1750, 600
@@ -69,42 +117,6 @@ window.mainloop()
 # 1760, 610
 
 
-# Old key handling code:
 
-# def move_square(event):
-#     if event.key == "ArrowLeft":
-#         player1.move(-10, 0)
-#     if event.key == "ArrowRight":
-#         player1.move(10,0)
-#     if event.key == "ArrowUp":
-#         player1.move(0, -10)
-#     if event.key == "ArrowDown":
-#         player1.move(0,10)
-
-# Circle Geometry Rubric ~
-#       pygame.draw.circle(surface, color, (center x, center y), radius, width)
-
-# player1 geometry:
-# pygame.draw.circle(screen, WHITE, (player1_x, player1_y), 10, 1)
-
-# Old key handling code below:
-
-#       add_key_down_handler(move_square)
-
-# New keyboard handing code:
-
-# with keyboard.Listener(on_press=move_square) as listener:
-#    listener.join()
-
-
-# def move_copter(event):
-#     if event.key == "a":
-#       player2.move(-10, 0)
-#    if event.key == "d":3
-#    player2.move(10, 0)
-#    if event.key == "w":
-#        player2.move(0, -10)
-#    if event.key == "s":
-#        player2.move(0, 10)
 
 
