8810ef9f46101c2694c305cfc40df3641c004439
Redfourk <turnej2096@g.comalisd.org>
2025-12-24 15:06:33 -0600

    Revert "Remove old barrier code."
    This reverts commit 899be948
    
    Signed-off-by: Redfourk <turnej2096@g.comalisd.org>

--- main_script.py
+++ main_script.py
@@ -26,11 +26,8 @@
 
 import tkinter as tk
 import threading
-import time
-
 from pyglet import event
 from pyglet.media import player
-from pygments.lexers import q
 
 # Create application window. App Name: "window"
 # Line below MUST be first tkinter related code to execute!
@@ -58,21 +55,15 @@ player1 = tk.Canvas.create_oval(background, player1_x0, player1_y0, player1_x1, 
 STEP = 10
 
 def move_player1(event):
-    dx, dy = 0, 0
     if event.keysym == "Up":
-        dx, dy = 0, -STEP
+        background.move(player1, 0, -STEP)
     elif event.keysym == "Down":
-        dx, dy = 0, STEP
+        background.move(player1, 0, STEP)
     elif event.keysym == "Left":
-        dx, dy = -STEP, 0
+        background.move(player1, -STEP, 0)
+        background.update_idletasks()
     elif event.keysym == "Right":
-        dx, dy = STEP, 0
-
-    background.move(player1, dx, dy)
-    if is_collision(player1):
-        background.move(player1, -dx, -dy)
-
-    background.update_idletasks()
+        background.move(player1, STEP, 0)
 
 window.bind('<Up>', move_player1)
 window.bind('<Down>', move_player1)
@@ -88,22 +79,15 @@ player2_y1 = 610
 player2 = tk.Canvas.create_oval(background, player2_x0, player2_y0, player2_x1, player2_y1, outline="#DE5844", fill="#DE5844", width=15)
 
 def move_player2(event):
-    dx, dy = 0, 0
     if event.keysym == "W" or event.keysym == "w":
-        dx, dy = 0, -STEP
+        background.move(player2, 0, -STEP)
     elif event.keysym == "S" or event.keysym == "s":
-        dx, dy = 0, STEP
+        background.move(player2, 0, STEP)
     elif event.keysym == "A" or event.keysym == "a":
-        dx, dy = -STEP, 0
+        background.move(player2, -STEP, 0)
         background.update_idletasks()
     elif event.keysym == "D" or event.keysym == "d":
-        dx, dy = STEP, 0
-
-    background.move(player2, dx, dy)
-    if is_collision(player2):
-        background.move(player2, -dx, -dy)
-
-    background.update_idletasks()
+        background.move(player2, STEP, 0)
 
 window.bind('<W>', move_player2)
 window.bind('<S>', move_player2)
@@ -117,26 +101,10 @@ window.bind('<d>', move_player2)
 
 # Barrier
 
-south_barrier = tk.Canvas.create_rectangle(background, 0, 1045, 2000, 1055, outline="#573c21", fill="#573c21", width=10)
-north_barrier = tk.Canvas.create_rectangle(background, 0, 0, 1900, 10, outline="#573c21", fill="#573c21", width=10)
-east_barrier = tk.Canvas.create_rectangle(background, 1900, 0, 1905, 1045, outline="#573c21", fill="#573c21", width=10)
+south_barrier = tk.Canvas.create_rectangle(background, 0, 1045, 2000, 1050, outline="#573c21", fill="#573c21", width=10)
+north_barrier = tk.Canvas.create_rectangle(background, 0, 0, 2000, 10, outline="#573c21", fill="#573c21", width=10)
+east_barrier = tk.Canvas.create_rectangle(background, 1902.5, 0, 1902.5, 1902.5, outline="#573c21", fill="#573c21", width=10)
 west_barrier = tk.Canvas.create_rectangle(background, 0, 0, 10, 1045, outline="#573c21", fill="#573c21", width=10)
-
-barriers = [south_barrier, north_barrier, east_barrier, west_barrier]
-
-
-def is_collision(player_id):
-    # Get current coordinates of the player (x0, y0, x1, y1)
-    coords = background.coords(player_id)
-    # Find all items overlapping with these coordinates
-    overlapping_items = background.find_overlapping(coords[0], coords[1], coords[2], coords[3])
-
-    # Check if any of the overlapping items are in our barriers list
-    for item in overlapping_items:
-        if item in barriers:
-            return True
-    return False
-
 
 # Old barrier code
 #
@@ -162,7 +130,6 @@ window.focus_set()
 label.pack()
 background.pack()
 window.mainloop()
-
 
 
 # 350, 200 out of 400x400
